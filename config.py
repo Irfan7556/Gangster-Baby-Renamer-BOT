996031d576d29af7273256618cc6d2fa94a77938
Mohammad Irfan <mohammad.irfan01239@gmail.com>
2025-05-06 14:00:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "21786970")
 
-API_HASH = os.environ.get("API_HASH", " aa1eaa84080fdf706c5cb37a27d35e81")
+API_HASH = os.environ.get("API_HASH", "aa1eaa84080fdf706c5cb37a27d35e81")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7849794082:AAEhUjqhZS-YMrWKgWghLbZnivv3MnRR_Lo") 
 
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', ' 5944299635').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5944299635').split()]
 
 PORT = os.environ.get("PORT", "8080")
